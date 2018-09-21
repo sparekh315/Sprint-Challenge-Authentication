872ff293950110f102d3b65bd371905a7d1ff78b
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-09-21 13:51:53 -0500

    removed serviceworker junk.

--- ClientSide/jokes/src/index.js
+++ ClientSide/jokes/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 
 
 ReactDOM.render(
@@ -11,4 +11,4 @@ ReactDOM.render(
 <App />
 </Router>, 
 document.getElementById('root'));
-registerServiceWorker();
+
